104915e7b5653419125fee5687ec1cc008d0bafc
Nurcan Cürebal <nurcancurebal@hotmail.com>
2023-01-11 23:11:35 +0300

    javascript-challenge-leaderboard

--- dom-javascript-challenge-leaderboard/main.js
+++ dom-javascript-challenge-leaderboard/main.js
@@ -1,61 +1,79 @@
 
 const players = document.querySelector("#players");
 
-function borderColor(inputsButtonColor) { // TODO yapamadım
+const PLAYERSOBJ = [
+    { firstName: "MARTHA", lastName: "YOHANES", country: "FINLAND", playerScore: "85" },
+    { firstName: "DAVID", lastName: "SMITH", country: "UNITED KINGDOM", playerScore: "80" },
+    { firstName: "NURCAN", lastName: "CUREBAL", country: "TURKEY", playerScore: "90" },
+    { firstName: "MATHIAS", lastName: "ELIAS", country: "SWEDEN", playerScore: "70" }
+];
 
-    inputsButtonColor.style.borderColor = "#814840";
+
+for (let index = 0; index < PLAYERSOBJ.length; index++) {
+
+    const playersDiv = document.createElement("div");
+    playersDiv.className = "players-div";
+
+
+    const playerSpanNameDate = document.createElement("span");
+
+    const playerDivName = document.createElement("div");
+
+    playerDivName.textContent = `${PLAYERSOBJ[index].firstName} ${PLAYERSOBJ[index].lastName}`;
+
+    const playerDivDate = document.createElement("div");
+
+    const date = new Date();
+
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let year = date.getFullYear();
+    let mounth = date.getMonth();
+    let dayNumber = date.getDate();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+
+    playerDivDate.innerText = `${months[mounth].toUpperCase()} ${dayNumber}.${year} ${hour}.${minute}`;
+
+    playerSpanNameDate.appendChild(playerDivName);
+    playerSpanNameDate.appendChild(playerDivDate);
+
+
+    const playerSpanCountry = document.createElement("span");
+    playerSpanCountry.textContent = `${PLAYERSOBJ[index].country}`;
+
+    const playerSpanScore = document.createElement("span");
+    playerSpanScore.textContent = `${PLAYERSOBJ[index].playerScore}`;
+
+
+    const playerSpanAction = document.createElement("span");
+    playerSpanAction.className = "buttons-action";
+
+    const deleteBorderDiv = document.createElement("div");
+    deleteBorderDiv.className = "buttons-parrent";
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa-solid fa-trash-can";
+    deleteBorderDiv.appendChild(deleteIcon);
+
+    const plusBorderDiv = document.createElement("div");
+    plusBorderDiv.className = "buttons-parrent";
+    const pointsPlusIcon = document.createElement("span");
+    pointsPlusIcon.innerText = "+5";
+    plusBorderDiv.appendChild(pointsPlusIcon);
+
+    const reduceBorderDiv = document.createElement("div");
+    reduceBorderDiv.className = "buttons-parrent";
+    const pointsReduceIcon = document.createElement("span");
+    pointsReduceIcon.innerText = "-5";
+    reduceBorderDiv.appendChild(pointsReduceIcon);
+
+    playerSpanAction.appendChild(deleteBorderDiv);
+    playerSpanAction.appendChild(plusBorderDiv);
+    playerSpanAction.appendChild(reduceBorderDiv);
+
+    playersDiv.appendChild(playerSpanNameDate);
+    playersDiv.appendChild(playerSpanCountry);
+    playersDiv.appendChild(playerSpanScore);
+    playersDiv.appendChild(playerSpanAction);
+
+    players.appendChild(playersDiv);
 }
-
-const playersDiv = document.createElement("div");
-playersDiv.style.backgroundColor = "#e5c9c5";
-playersDiv.style.display = "flex";
-playersDiv.style.justifyContent = "space-between";
-playersDiv.style.width = "978px";
-playersDiv.style.height = "60px";
-playersDiv.style.lineHeight = "60px";
-
-const playerSpanName = document.createElement("span");
-playerSpanName.textContent = "MARTHA YOHANES";
-playerSpanName.style.marginLeft = "15px";
-
-const playerSpanCountry = document.createElement("span");
-playerSpanCountry.textContent = "FINLAND";
-
-const playerSpanScore = document.createElement("span");
-playerSpanScore.textContent = "85";
-
-const playerSpanAction = document.createElement("span");
-playerSpanAction.className = "buttons-action"
-
-const deleteBorderDiv = document.createElement("div");
-deleteBorderDiv.className = "buttons-parrent";
-
-const deleteIcon = document.createElement("i");
-deleteIcon.className = "fa-solid fa-trash-can";
-
-deleteBorderDiv.appendChild(deleteIcon);
-
-const plusBorderDiv = document.createElement("div");
-plusBorderDiv.className = "buttons-parrent";
-
-const pointsPlusIcon = document.createElement("span");
-pointsPlusIcon.innerText = "+5";
-plusBorderDiv.appendChild(pointsPlusIcon);
-
-const reduceBorderDiv = document.createElement("div");
-reduceBorderDiv.className = "buttons-parrent";
-
-const pointsReduceIcon = document.createElement("span");
-pointsReduceIcon.innerText = "-5";
-reduceBorderDiv.appendChild(pointsReduceIcon);
-
-playerSpanAction.appendChild(deleteBorderDiv);
-playerSpanAction.appendChild(plusBorderDiv);
-playerSpanAction.appendChild(reduceBorderDiv);
-
-playersDiv.appendChild(playerSpanName);
-playersDiv.appendChild(playerSpanCountry);
-playersDiv.appendChild(playerSpanScore);
-playersDiv.appendChild(playerSpanAction);
-
-players.appendChild(playersDiv);
